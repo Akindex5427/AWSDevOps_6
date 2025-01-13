8835c2f97b506e027cdba6e71023ef6fa3d70401
Akindex5427 <56794552+Akindex5427@users.noreply.github.com>
2025-01-13 16:07:33 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const path = require("path");
 dotenv.config();
 
 const port = process.env.PORT || 8080;
-const HOST = process.env.HOST || '0.0.0.0';
+// const HOST = process.env.HOST || '0.0.0.0';
 const mongodbUrl = config.MONGODB_URL;
 
 mongoose
