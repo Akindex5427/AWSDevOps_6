3243da0d9887193a8e523f22bc783d33b13d0c35
Akindex5427 <56794552+Akindex5427@users.noreply.github.com>
2025-01-12 23:36:32 -0600

    Update install.sh

--- deploy_scripts/install.sh
+++ deploy_scripts/install.sh
@@ -1,6 +1,6 @@
 # Install Node.js and npm for Debian-based systems
 curl -sL https://deb.nodesource.com/setup_lts.x | bash -
-apt-get install -y nodejs
+sudo apt-get install nodejs -y
 
 # install pm2 module globaly
 npm install -g pm2
